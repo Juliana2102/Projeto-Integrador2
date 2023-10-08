ec36d78de7dfdb615e6667d03e2c8b93e65ec15d
Juiana2102 <2102502@aluno.univesp.br>
2023-10-07 22:22:43 -0300

    Alterações de testes

--- public/routes.js
+++ public/routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./db");
 const upload = require("./multerConfig");
+const routes = require("./routes");
 
 const app = express();
 const port = 3000;
