30eab17a94c617a4e0e65b0d48cc6bb4467f4839
Juliana2102 <2102502@aluno.univesp.br>
2023-10-27 20:15:29 -0300

    Atualizações e conexão com o banco de dados

---
+++ public/INSERT INTO cadastro (nome, senhaCadastr.sql
@@ -0,0 +1,4 @@
+INSERT INTO cadastro (nome, senhaCadastro, email) VALUES
+('Nome1', 'Senha1', 'email1@example.com'),
+('Nome2', 'Senha2', 'email2@example.com'),
+('Nome3', 'Senha3', 'email3@example.com');
